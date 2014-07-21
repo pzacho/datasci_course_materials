2cad75e2f0cbcee725253c7119e65e415297084e
Per Zacho <pzacho@gmail.com>
2014-07-21 14:21:20 +0200

    added problem 2

---
+++ assignment2/problem2.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sqlite3 matrix.db < problem2.sql > multiply.txt
---
+++ assignment2/problem2.sql
@@ -0,0 +1,2 @@
+.headers off
+select /*a.row_num, b.col_num,*/ sum(a.value*b.value) from a,b where a.col_num=b.row_num and a.row_num=2 and b.col_num=3 group by a.row_num, b.col_num;
